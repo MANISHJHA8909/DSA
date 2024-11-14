424e061ec4461858e891933406a571eef7ab002e
MANISH KUMAR JHA <manishjha8909@gmail.com>
2024-11-14 19:15:32 +0530

    code C

---
+++ Atcoder_DP/Vacation.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Vacation {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][3];
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<3; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        
+
+        sc.close();
+    }
+}
