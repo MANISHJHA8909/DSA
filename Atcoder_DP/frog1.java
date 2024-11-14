128b080150a7cec745ab9bd380e4b98740ba6397
MANISH KUMAR JHA <manishjha8909@gmail.com>
2024-11-14 18:09:23 +0530

    Initial Build

---
+++ Atcoder_DP/frog1.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class frog1 { 
+    static int k = 2; 
+    static int[] dp = new int[100001]; 
+
+    static int solve(int i, int[] arr) {
+        if (i >= arr.length - 1) return 0;
+
+        if (dp[i] != -1) return dp[i]; 
+
+        int ans = Integer.MAX_VALUE; 
+
+        for (int jmp = 1; jmp <= k; jmp++) {
+            int j = i + jmp;
+            if (j < arr.length) { 
+                ans = Math.min(ans, Math.abs(arr[i] - arr[j]) + solve(j, arr));
+            }
+        }
+
+        dp[i] = ans; 
+        return ans;
+    }
+
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.fill(dp, -1); 
+
+        int ans = solve(0, arr);
+        System.out.println(ans);
+        sc.close();
+    } 
+}
