8beb6c113d7ff7ce0d3f652d0bce4edf954121bd
MANISH KUMAR JHA <manishjha8909@gmail.com>
2024-11-14 23:50:01 +0530

    Bottom_up

--- Atcoder_DP/C_Vacation.java
+++ Atcoder_DP/C_Vacation.java
@@ -16,40 +16,45 @@ public class C_Vacation {
             }
         }
 
-        // starting position first row all elements
+        for(int i=n-2; i>=0; i--) {
+            int v0 = arr[i+1][0];
+            int v1 = arr[i+1][1];
+            int v2 = arr[i+1][2];
+            for(int j=2; j>=0; j--) {
+                if(j==2) {
+                    arr[i][j] = Math.max(v0, v1) + arr[i][j];
+                } else if(j==1) {
+                    arr[i][j] = Math.max(v0, v2) + arr[i][j];
+                } else {
+                    arr[i][j] = Math.max(v2, v1) + arr[i][j];
+                }
+            }
+        }
         int ans = Integer.MIN_VALUE;
-         
-
-        int val1 = solve(arr, 0, n, 0);
-        int val2 = solve(arr, 1, n, 0);
-        int val3 = solve(arr, 2, n, 0); 
-
-        //System.out.println(val1 +" "+val2+" " +val3);
-
-        ans = Math.max(ans, val1);
-        ans = Math.max(ans, val2);
-        ans = Math.max(ans, val3);
-
+        for(int i=0; i<3; i++) {
+            ans = Math.max(ans, arr[0][i]);
+        }
+        
         System.out.println(ans);
         sc.close();
     }
 
-    static int solve(int[][] arr, int ind, int n, int row) {
-        if(row >= n) {
-            return 0;
-        }
+    // static int solve(int[][] arr, int ind, int n, int row) {
+    //     if(row >= n) {
+    //         return 0;
+    //     }
 
-        if(dp[row][ind] != -1) return dp[row][ind];
+    //     if(dp[row][ind] != -1) return dp[row][ind];
 
-        int ans = Integer.MIN_VALUE;
+    //     int ans = Integer.MIN_VALUE;
 
-        for(int j=0; j<3; j++) {
-            if(j!=ind) {
-                ans = Math.max(arr[row][ind] + solve(arr, j, n, row+1) ,ans);
-            }
-        }
+    //     for(int j=0; j<3; j++) {
+    //         if(j!=ind) {
+    //             ans = Math.max(arr[row][ind] + solve(arr, j, n, row+1) ,ans);
+    //         }
+    //     }
 
-        dp[row][ind] = ans;
-        return ans;
-    }
+    //     dp[row][ind] = ans;
+    //     return ans;
+    // }
 }
