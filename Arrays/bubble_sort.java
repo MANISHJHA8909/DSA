08a2b414e89a64d14501a6ed9cdf8be678c03390
MANISHJHA8909 <manishjha8909@gmail.com>
2025-08-02 08:18:04 +0530

    Add bubble sort and insertion sort implementations in Java

---
+++ Arrays/bubble_sort.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+import java.util.*;
+
+/* Bubble sort works by  */
+public class bubble_sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        bubble_sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubble_sort(int arr[]) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no two elements were swapped by inner loop, then break
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
---
+++ Arrays/insertion_sort.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.*;
+
+public class insertion_sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        insertion_sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertion_sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+}
